933bab69fcf3492da05d1b5574bcbf4056c2e4ed
Tyler <j0h@example.com>
2019-12-03 20:39:44 -0600

    1

--- datastuff.py
+++ datastuff.py
@@ -99,5 +99,3 @@ non_udacity_submissions = take_out_udacity(project_submissions)
 print(len(non_udacity_submissions))
 
 LESSON 14######################################################################################################################################################
-
-print('hello world')
