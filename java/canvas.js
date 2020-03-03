ba0d4bf54e5108eb591bcf453ce468a295af659d
Tyler <j0h@example.com>
2020-03-02 20:00:47 -0600

    change

--- java/canvas.js
+++ java/canvas.js
@@ -7,6 +7,17 @@ var c = canvas.getContext('2d');
 var gravity =1;
 var friction = .80;
 
+let mouse = {
+    x: innerWidth / 2,
+    y: innerHeight / 2
+};
+
+
+addEventListener('mousemove', function(event) {
+    mouse.x = event.clientX;
+    mouse.y = event.clientY
+});
+
 function random(max){
     var any = Math.floor(Math.random() * max)
     return any
@@ -20,8 +31,6 @@ class ball {
         this.y = y;
         this.radias = radias;
         this.color = color;
-        //this.dy = dy;
-        //this.update();
     }
     draw () {
         c.beginPath();
@@ -37,7 +46,6 @@ class gravityBall extends ball {
         super(x,y,radias,color);
         this.dy = dy;
         this.update()
-
     }
     update() {
         if (this.y + this.radias > canvas.height ) {
@@ -49,20 +57,34 @@ class gravityBall extends ball {
         this.y += this.dy;
         this.draw()
     };
-}
+};
 
+class mouseBall extends ball {
+    constructor (x,y,radias,color) {
+        super(x,y,radias,color)
+        this.update()
+    }
+    update() {
+        this.x = mouse.x;
+        this.y = mouse.y;
+        this.draw()
+    }
+};
+
+let mouseCircle;
 var balls;
 var ballArray = [];
 function init() {
-    for (var i = 0; i < 100; i++) {
+    mouseCircle = new mouseBall(undefined,undefined,50,'green')
+    /*=for (var i = 0; i < 100; i++) {
         var ranx = random(canvas.width);
         var rany = random(canvas.height)-75;
         var ranradias = random(75);
         var rancolor = 'rgb('+random(255)+', '+random(255)+', '+random(255)+')'
         var ranspeed = ranrange(30,30)
         ballArray.push(new gravityBall(ranx,rany,ranradias,rancolor,ranspeed))
-    };
-    //balls = new ball(300,canvas.height-51,50,'red',10)
+    };*/
+    balls = new gravityBall(300,canvas.height-700,50,'red',10)
 };
 
 
@@ -71,11 +93,10 @@ function animate() {
     c.clearRect(0,0,canvas.width,canvas.height);
     for (var i = 0; i < ballArray.length; i++) {
         ballArray[i].update();
-
     };
-
+    mouseCircle.update()
+    balls.update();
 };
 
 init();
 animate();
-
